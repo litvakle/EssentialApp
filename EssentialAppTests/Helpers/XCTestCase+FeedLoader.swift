fa56b17f47dd76ea65c01a1031073f84dd04eaea
Litvak Lev <sn.delui@gmail.com>
2022-08-25 18:27:00 +0500

    Extract 'FeedLoader' test helpers into a shared scope to remove duplication

---
+++ EssentialAppTests/Helpers/XCTestCase+FeedLoader.swift
@@ -0,0 +1,33 @@
+//
+//  XCTestCase+FeedLoader.swift
+//  EssentialAppTests
+//
+//  Created by Lev Litvak on 25.08.2022.
+//  Copyright © 2022 Essential Developer. All rights reserved.
+//
+
+import XCTest
+import EssentialFeed
+
+protocol FeedLoaderTestCase: XCTestCase {}
+
+extension FeedLoaderTestCase {
+    func expect(_ sut: FeedLoader, toCompleteWith expectedResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Wait for load completion")
+        
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedFeed), .success(expectedFeed)):
+                XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
+            case (.failure, .failure):
+                break
+            default:
+                XCTFail("Expected to complete with \(expectedResult), got \(receivedResult) instead")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+}
