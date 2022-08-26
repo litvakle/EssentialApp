a9724a5e5ac6bd98e2778fd25e224e8306204584
Litvak Lev <sn.delui@gmail.com>
2022-08-26 12:34:23 +0500

    Add test target for running high level acceptance tests

--- EssentialApp/SceneDelegate.swift
+++ EssentialApp/SceneDelegate.swift
@@ -13,24 +13,28 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 		guard let _ = (scene as? UIWindowScene) else { return }
 		
-        let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-
-        let session = URLSession(configuration: .ephemeral)
-        let client = URLSessionHTTPClient(session: session)
-        let remoteFeedLoader = RemoteFeedLoader(url: url, client: client)
-        let RemoteImageLoader = RemoteFeedImageDataLoader(client: client)
-
-        let localStoreURL = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("feed-store.sqlite")
-        let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
-        let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
-        let localImageLoader = LocalFeedImageDataLoader(store: localStore)
-        
-        let feedViewController = FeedUIComposer.feedComposedWith(
-            feedLoader: FeedLoaderWithFallbackComposite(
-                primary: remoteFeedLoader, fallback: localFeedLoader),
-            imageLoader: FeedImageDataLoaderWithFallbackComposite(
-                primary: RemoteImageLoader, fallback: localImageLoader))
-        
-        window?.rootViewController = feedViewController
+//        let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+//
+//        let session = URLSession(configuration: .ephemeral)
+//        let client = URLSessionHTTPClient(session: session)
+//        let remoteFeedLoader = RemoteFeedLoader(url: url, client: client)
+//        let RemoteImageLoader = RemoteFeedImageDataLoader(client: client)
+//
+//        let localStoreURL = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("feed-store.sqlite")
+//        let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
+//        let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
+//        let localImageLoader = LocalFeedImageDataLoader(store: localStore)
+//
+//        window?.rootViewController = FeedUIComposer.feedComposedWith(
+//            feedLoader: FeedLoaderWithFallbackComposite(
+//                primary: FeedLoaderCacheDecorator(
+//                    decoratee: remoteFeedLoader,
+//                    cache: localFeedLoader),
+//                fallback: localFeedLoader),
+//            imageLoader: FeedImageDataLoaderWithFallbackComposite(
+//                primary: localImageLoader,
+//                fallback: FeedImageDataLoaderCacheDecorator(
+//                    decoratee: RemoteImageLoader,
+//                    cache: localImageLoader)))
 	}
 }
