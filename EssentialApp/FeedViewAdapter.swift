aa86b1693787c2db2b3c3ec214dc01e924bba344
Litvak Lev <sn.delui@gmail.com>
2022-09-03 15:11:29 +0500

    Configure 'ErrorView' programmatically, so we don't duplicate layout logic in storyboards

--- EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/FeedViewAdapter.swift
@@ -36,7 +36,7 @@ final class FeedViewAdapter: ResourceView {
                     return image
                 })
 			
-			return CellController(view, view, view)
+			return CellController(view)
 		})
 	}
 }
