4f758ef3b4bf84e4997f12d1ffaa653bedc32572
Litvak Lev <sn.delui@gmail.com>
2022-09-04 13:58:52 +0500

    Use diffable datasource to only reload cells when needed

--- EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/FeedViewAdapter.swift
@@ -36,7 +36,7 @@ final class FeedViewAdapter: ResourceView {
                     return image
                 })
 			
-			return CellController(view)
+            return CellController(id: model, view)
 		})
 	}
 }
