d021e974a0b2b4323c5faa62bf2d62294fe0a315
Litvak Lev <sn.delui@gmail.com>
2022-09-05 17:17:27 +0500

    Notifies selection handler on image selection

--- EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/FeedViewAdapter.swift
@@ -10,11 +10,17 @@ import EssentialFeediOS
 final class FeedViewAdapter: ResourceView {
 	private weak var controller: ListViewController?
 	private let imageLoader: (URL) -> FeedImageDataLoader.Publisher
+    private let selection: (FeedImage) -> Void
 	
-	init(controller: ListViewController, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) {
-		self.controller = controller
-		self.imageLoader = imageLoader
-	}
+	init(
+        controller: ListViewController,
+        imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher,
+        selection: @escaping (FeedImage) -> Void
+    ) {
+        self.controller = controller
+        self.imageLoader = imageLoader
+        self.selection = selection
+    }
 	
 	func display(_ viewModel: FeedViewModel) {
 		controller?.display(viewModel.feed.map { model in
@@ -25,7 +31,11 @@ final class FeedViewAdapter: ResourceView {
             
             let view = FeedImageCellController(
                 viewModel: FeedImagePresenter.map(model),
-                delegate: adapter)
+                delegate: adapter,
+                selection: { [selection] in
+                    selection(model)
+                }
+            )
 			
 			adapter.presenter = LoadResourcePresenter(
 				resourceView: WeakRefVirtualProxy(view),
